29d5afaf287732df4647d06e2fe217c7c8e2aceb
Marcelo Bueno <marcelobueno25@hotmail.com>
2020-04-21 01:14:21 -0300

    Ajuste no Routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Routes from './routes/routes';
 import './index.css';
-import App from './App';
-import Home from './pages/Home';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact={true} component={App} />
-      <Route path="/home" component={Home} />
-    </Switch>
-  </ BrowserRouter>
-  , document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
