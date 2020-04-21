911090cb3172693839e6db6948c0df51a93e60be
Marcelo Bueno <marcelobueno25@hotmail.com>
2020-04-21 00:18:35 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Realizando o primeiro teste</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Box/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Box extends React.Component {
+  render() {
+    return (
+      <p>Box</p>
+    )
+  };
+}
